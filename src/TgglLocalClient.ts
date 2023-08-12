585f90d8e2d544ca6c335cfc60a0dbdf8c6716a1
nick-keller <nicolas.kllr@gmail.com>
2023-08-12 16:40:55 +0200

    Add on config change callback

--- src/TgglLocalClient.ts
+++ src/TgglLocalClient.ts
@@ -12,6 +12,10 @@ export class TgglLocalClient<
   private pollingInterval?: number
   private timeoutID?: ReturnType<typeof setTimeout>
   private fetchID = 0
+  private onConfigChangeCallbacks = new Map<
+    number,
+    (config: Map<TgglFlagSlug<TFlags>, Flag>) => void
+  >()
 
   constructor(
     private apiKey: string,
@@ -26,6 +30,15 @@ export class TgglLocalClient<
     this.url = options.url ?? 'https://api.tggl.io/config'
     this.config = options.initialConfig ?? new Map()
     this.pollingInterval = options.pollingInterval
+  }
+
+  onConfigChange(callback: (config: Map<TgglFlagSlug<TFlags>, Flag>) => void) {
+    const id = Math.random()
+    this.onConfigChangeCallbacks.set(id, callback)
+
+    return () => {
+      this.onConfigChangeCallbacks.delete(id)
+    }
   }
 
   startPolling(pollingInterval: number) {
@@ -66,9 +79,24 @@ export class TgglLocalClient<
         }, this.pollingInterval)
       }
 
+      const configChanged =
+        this.onConfigChangeCallbacks.size > 0 &&
+        (response.length !== this.config.size ||
+          !response.every(
+            (flag: any) =>
+              JSON.stringify(this.config.get(flag.slug)) ===
+              JSON.stringify(flag)
+          ))
+
       this.config.clear()
       for (const flag of response) {
         this.config.set(flag.slug, flag)
+      }
+
+      if (configChanged) {
+        for (const callback of this.onConfigChangeCallbacks.values()) {
+          callback(this.config)
+        }
       }
     } catch (error) {
       if (this.pollingInterval && this.pollingInterval > 0 && !this.timeoutID) {
