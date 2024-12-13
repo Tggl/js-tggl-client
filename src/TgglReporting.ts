a340e9511260421d45714ed9e5ef7bec17e43d3a
nick-keller <nicolas.kllr@gmail.com>
2024-12-13 09:22:10 +0100

    2.0.1

--- src/TgglReporting.ts
+++ src/TgglReporting.ts
@@ -1,7 +1,7 @@
 import { apiCall } from './apiCall'
 import { TgglReportingOptions } from './types'
 
-export const PACKAGE_VERSION = '2.0.0'
+export const PACKAGE_VERSION = '2.0.1'
 
 const constantCase = (str: string) => {
   return str
